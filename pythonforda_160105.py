811cd42173e3767cfd1a6be508e620c7ffa6978f
Chmwang <chm.wang@icloud.com>
2016-01-05 06:51:32 +0100

    learn
    leanpy

---
+++ pythonforda_160105.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=5
+
+import numpy as np
+import pandas as pd
+import matplotlib as mp
+import IPython as ip
+import scipy as sp
+
+numpy.random()
+randn()
+
+
+
+from numpy.random import randn
+data={i:randn() for i in range(7)}
+ 
+data?
+
+
+an_apple=27
+an_example=42
+
+an_apple
+
+b=[1,2,3]
+b?
+
+import datetime
+
+datetime.date
+
+def add_numbers(a,b):
+    return a+b
+
+add_numbers(1,8)
+add_numbers?
+add_numbers??
+
+
+
+
+
